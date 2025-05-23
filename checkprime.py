015fc21165e019c41466d7325d4ac90a8d6cf212
ashu304-ops <ashuwarrior304@gmail.com>
2025-05-23 14:13:50 +0530

    Update checkprime.py

--- checkprime.py
+++ checkprime.py
@@ -25,7 +25,7 @@ def checkprime(n):
                 return False 
     return True
 
-if checkprime(15)==True:
+if checkprime(14)==True:
     print('it  is  a  prime number')
 else :
     print('it is not  prime  number')   
